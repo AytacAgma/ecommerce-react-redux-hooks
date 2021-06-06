8b5a8f4f3e440cf5b42f3b98bc0c34d7fffe187a
Aytac Agma <aytac.agma@netone.net.tr>
2021-06-06 18:25:26 +0300

    Page constructions were changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Dashboard from './layouts/Dashboard';
-import 'semantic-ui-css/semantic.min.css'
+import "./App.css";
+import Dashboard from "./layouts/Dashboard";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import Navi from "./layouts/Navi";
 
 function App() {
   return (
-    <divnpm className="App">
-      <Dashboard/>
-    </divnpm >
+    <div className="App">
+      <Navi />
+      <Container className="main">
+        <Dashboard />
+      </Container>
+    </div>
   );
 }
 
