7a4ee102969488f5975d047f59c4af7104b9282d
Aytac Agma <aytac.agma@netone.net.tr>
2021-06-06 16:20:16 +0300

    Navigation panel was implemented with the power of Semantic UI

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Button, Dropdown, Menu } from "semantic-ui-react";
 
 export default function Navi() {
-    return (
-        <div>
-            Navi
-        </div>
-    )
+  return (
+    <div>
+      <Menu inverted fixed>
+        <Menu.Item
+          name="home"
+        />
+        <Menu.Item
+          name="messages"
+        />
+
+        <Menu.Menu position="right">
+          <Dropdown item text="Language">
+            <Dropdown.Menu>
+              <Dropdown.Item>English</Dropdown.Item>
+              <Dropdown.Item>Russian</Dropdown.Item>
+              <Dropdown.Item>Spanish</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Menu.Item>
+            <Button primary>Sign Up</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
 }
