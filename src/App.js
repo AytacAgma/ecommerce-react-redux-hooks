5129932810fcd9af24f334273df1f74544672e3c
Aytac Agma <aytac.agma@netone.net.tr>
2021-06-06 15:07:31 +0300

    semantic-ui was added and js extensions were changed to jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Dashboard from './layouts/Dashboard';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <divnpm className="App">
       <Dashboard/>
-    </div>
+    </divnpm >
   );
 }
 
