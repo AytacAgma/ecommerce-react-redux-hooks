57757503574f0d4ec392f59c38ec0b18902d2101
Aytac Agma <aytac.agma@netone.net.tr>
2021-06-19 16:06:54 +0300

    toastify was added and alerts were implemented. addToCart operation was implemented

--- src/layouts/CartSummary.jsx
+++ src/layouts/CartSummary.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, Label } from "semantic-ui-react";
 
 export default function CartSummary() {
+
+  const {cartItems} = useSelector(state => state.cart)
     return (
         <div>
             <Dropdown item text="Language">
               <Dropdown.Menu>
-                <Dropdown.Item>Acer</Dropdown.Item>
-                <Dropdown.Item>Asus</Dropdown.Item>
-                <Dropdown.Item>Dell</Dropdown.Item>
+                {
+                  cartItems.map(cartItem=>(
+                  <Dropdown.Item key={cartItem.product.id}>
+                    {cartItem.product.productName}
+                    <Label>
+                      {cartItem.quantity}
+                    </Label>
+                  </Dropdown.Item>
+                  ))
+                }
+                
                 <Dropdown.Divider/>
                 <Dropdown.Item as={NavLink} to="/cart">Sepete Git</Dropdown.Item>
               </Dropdown.Menu>
--- src/store/initialValues/cartItems.js
+++ src/store/initialValues/cartItems.js
@@ -1,1 +1,1 @@
-export const cartItems=[{quantity:1, product:{productName:"Kek"}}]
+export const cartItems=[]
